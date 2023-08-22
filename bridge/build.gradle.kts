7fc9bfc5a7a332f5e8b5e0a33294cd111fc4cc74
Victor Kabata <victorbro14@gmail.com>
2023-08-22 11:53:32 +0300

    Removed daraja module

--- bridge/build.gradle.kts
+++ bridge/build.gradle.kts
@@ -13,20 +13,12 @@ plugins {
 
 val dokkaOutputDir = buildDir.resolve("reports/dokka")
 
-val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
     targetHierarchy.default()
 
     android {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = JavaVersion.VERSION_1_8.toString()
-            }
-            publishLibraryVariants("release", "debug")
-        }
+        publishLibraryVariants("release", "debug")
     }
 
     iosX64()
@@ -94,76 +86,6 @@ val javadocJar = tasks.register<Jar>("javadocJar") {
     from(dokkaOutputDir)
 }
 
-/*
-publishing {
-
-    repositories {
-        maven {
-            name = "Sonatype"
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl
-            else releasesRepoUrl
-
-            credentials {
-                username = project.get("OSSRH_USERNAME")
-                password = project.get("OSSRH_PASSWORD")
-            }
-        }
-    }
-
-    publications.withType<MavenPublication> {
-
-        artifact(javadocJar)
-
-        pom {
-            groupId = project.get("POM_GROUPID")
-            artifactId = project.get("POM_ARTIFACTID")
-            version = project.get("POM_VERSION")
-
-            name.set(project.get("POM_NAME"))
-            description.set(project.get("POM_DESCRIPTION"))
-            url.set(project.get("POM_URL"))
-
-            developers {
-                developer {
-                    id.set(project.get("POM_DEVELOPER_ID"))
-                    name.set(project.get("POM_DEVELOPER_NAME"))
-                    email.set(project.get("POM_DEVELOPER_EMAIL"))
-                }
-            }
-
-            licenses {
-                license {
-                    name.set(project.get("POM_LICENSE_NAME"))
-                    url.set(project.get("POM_LICENSE_URL"))
-                }
-            }
-
-            issueManagement {
-                system.set(project.get("POM_ISSUE_SYSTEM"))
-                url.set(project.get("POM_ISSUE_URL"))
-            }
-
-            scm {
-                connection.set(project.get("POM_SCM_CONNECTION"))
-                developerConnection.set(project.get("POM_SCM_DEVELOPER_CONNECTION"))
-                url.set(project.get("POM_SCM_URL"))
-            }
-        }
-    }
-
-    signing {
-        val signingKeyId = project.get("SIGNING_ID")
-        val signingKeyPassword = project.get("SIGNING_KEY")
-        val signingKey = project.get("SIGNING_PASSWORD")
-
-        useInMemoryPgpKeys(
-            signingKeyId, signingKeyPassword, signingKey
-        )
-        sign(publishing.publications)
-    }
-}
-*/
-
 /*addGithubPackagesRepository()
 kmmbridge {
     frameworkName.set("DarajaMultiplatform")
@@ -172,12 +94,3 @@ kmmbridge {
     versionPrefix.set("0.2")
     spm()
 }*/
-
-// Opt-In Experimental ObjCName in Kotlin > 1.8.0
-kotlin.sourceSets.all {
-    languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
-}
-
-task("testClasses").doLast {
-    println("This is a dummy testClasses task")
-}
